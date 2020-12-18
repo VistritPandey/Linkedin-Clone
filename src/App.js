78832d5f082b02255d70ee5b08e22d3fc2f2e541
vistritpandey <vistritpandey@gmail.com>
2020-12-18 19:57:23 +0530

    Feed added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
+import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <Header /> 
+      <Header />
       <div className="app__body">
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
---
+++ src/Feed.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <h1>This is where the feed will come</h1>
+    </div>
+  );
+}
+
+export default Feed;
