2de574bd8667bf89948c7a5196338d497fca4fab
vistritpandey <vistritpandey@gmail.com>
2020-12-18 14:10:09 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello there</h1>
+      <Header /> 
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Header</h1>
+            <div className="header__left">
+                <img src="" alt="" />
+
+                <div className="header__search">
+                    <input type="text" />
+                </div>
+            </div>
+
+            <div className="header__right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
