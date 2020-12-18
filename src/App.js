3b261f9a7a01507432edaad9a0fe4bb3b75db503
vistritpandey <vistritpandey@gmail.com>
2020-12-18 18:43:19 +0530

    Sidebar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header /> 
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>Sidebar</h1>
+        </div>
+    )
+}
+
+export default Sidebar
