fb6e2f2d55863f226004ba0e9b34d597c196e246
vistritpandey <vistritpandey@gmail.com>
2020-12-19 07:06:07 +0530

    input

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import InputOption from "./InputOption";
 
 function Feed() {
   return (
     <div className="feed">
-      <h1>This is where the feed will come</h1>
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" placeholder="Post something" />
+            <button className="light" type="submit">
+              Post
+            </button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/InputOption.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./InputOption.css";
+function InputOption() {
+  return (
+    <div className="inputOption">
+      <h1>Input</h1>
+    </div>
+  );
+}
+
+export default InputOption;
