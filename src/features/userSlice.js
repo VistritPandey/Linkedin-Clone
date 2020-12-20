a7bcedd1529876075be0da742ac4da25dccfb89f
vistritpandey <vistritpandey@gmail.com>
2020-12-20 09:42:20 +0530

    userSlice

---
+++ src/features/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = userSlice.actions;
+export const selectCount = (state) => state.counter.value;
+
+export default userSlice.reducer;
