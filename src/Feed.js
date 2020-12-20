23f07366fd9390394e2c9fbcc99961e92cbac0a4
vistritpandey <vistritpandey@gmail.com>
2020-12-20 09:30:57 +0530

    FEED

--- src/Feed.js
+++ src/Feed.js
@@ -15,24 +15,25 @@ function Feed() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) =>
-      setPosts(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data(),
-        }))
-      )
-    );
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
   }, []);
 
   const sendPost = (e) => {
     e.preventDefault();
     db.collection("posts").add({
-      name: "Vistrit",
-      description: "STONKS!!!!!!",
+      name: "Dummy",
+      description: "description",
       message: input,
-      photoUrl:
-        "https://lh3.googleusercontent.com/ogw/ADGmqu9jlTtiVnYKEPl0kL5mXmjMil3nzlUpJ1XwTZpZ5g=s192-c-mo",
+      photoUrl: "",
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
     setInput("");
