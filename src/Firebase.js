47259f2cb246d4c667ac19de98ab459071fddfd2
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-01-04 21:08:40 -0500

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAmdDZnHTHWZbezrht_5BRmuCucZiZqlTQ",
-  authDomain: "vistrit-linkedin.firebaseapp.com",
-  projectId: "vistrit-linkedin",
-  storageBucket: "vistrit-linkedin.appspot.com",
-  messagingSenderId: "612816959798",
-  appId: "1:612816959798:web:a400ed9f64455a01451b97",
-  measurementId: "G-C163XDYGEF",
-};
+const firebaseConfig = {/*Use your own Config*/};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
